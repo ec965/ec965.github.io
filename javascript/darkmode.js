7f5d3684fa267cc13d0e105c89ea902fc4d10dea
Enoch <enoch965@gmail.com>
2020-05-13 12:36:36 -0700

    dark mode card shadows

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -36,44 +36,6 @@ function dark_mode_storage() {
   }
 }
 
-/*change body and navbar to dark mode*/
-// function dark_mode_storage() {
-//   var body_element = document.getElementsByTagName('body');
-//   if(localStorage.getItem('darkmode') === 'ON'){
-//     body_element[0].classList.add('dark-body');
-//   }
-//   else if(localStorage.getItem('darkmode') === null){
-//     body_element[0].classList.remove('dark-body');
-//   }
-// }
-
-// function dm_navbar(){
-//   var navbar = document.getElementById("navbar1");
-//   if(localStorage.getItem('darkmode') === 'ON'){
-//     navbar.classList.add('navbar-dark');
-//     navbar.classList.add('bg-primary');
-//     navbar.classList.remove('navbar-light');
-//     navbar.classList.remove('bg-light');
-//   }
-//   else if(localStorage.getItem('darkmode') === null){
-//     navbar.classList.remove('navbar-dark');
-//     navbar.classList.remove('bg-primary');
-//     navbar.classList.add('navbar-light');
-//     navbar.classList.add('bg-light');
-//   }
-// }
-//
-// /*darkmode for the toggle checkbox*/
-// function dm_toggle_storage(){
-//   var toggle_state = document.getElementById("dark_toggle");
-//   if(localStorage.getItem('darkmode') === 'ON'){
-//     toggle_state.checked = true;
-//   }
-//   else if(localStorage.getItem('darkmode') === null){
-//     toggle_state.checked = false;
-//   }
-// }
-
 /*dark mode for specific page elemnet classes*/
 function dm_element(element_class){
   var element = document.getElementsByClassName(element_class);
@@ -85,6 +47,21 @@ function dm_element(element_class){
   else if(localStorage.getItem('darkmode') === null){
     for(var i=0; i<element.length; i++){
       element[i].classList.remove('dark');
+    }
+  }
+}
+
+//change box shadows for cards and hover element
+function dm_card_shadow(){
+  var element = document.getElementsByClassName('card');
+  if(localStorage.getItem('darkmode') === 'ON'){
+    for(var i=0; i<element.length; i++){
+      element[i].classList.add('card-shadow-dark');
+    }
+  }
+  else if(localStorage.getItem('darkmode') === null){
+    for(var i=0; i<element.length; i++){
+      element[i].classList.remove('card-shadow-dark');
     }
   }
 }
