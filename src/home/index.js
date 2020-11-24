84a82e510bcb22a76528548b116d3887c8d74093
ec965 <enoch965@gmail.com>
2020-11-23 16:00:55 -0800

    update home

--- src/home/index.js
+++ src/home/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
-//bootstrap
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-//my stuff
-import SlideShow from './slide';
-import { Link } from 'react-router-dom';
 
 export default function Home (props){
   return(
-    <Container>
-      <Row>
-        <Col>
-          <Link to='/projects'> 
-            <SlideShow/>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
+    <div className="cover">
+      <h1 className="cover-text">Cover Page.</h1>
+    </div>
   );
 }
 
