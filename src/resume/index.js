86da6a56c3f7aabe445609002e8fe5d6b610b44e
ec965 <enoch965@gmail.com>
2020-11-23 23:39:03 -0800

    new resume page

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-function ResumePage(props){
+function ResumeObject(props){
   return(
-    <Container className="pageFade">
-      <Row>
-        <h2>Resume</h2>
-      </Row>
-      <Row>
-        <object data={props.data} type="application/pdf" width="100%" height="800px">
-          <p>It appears you don't have a PDF plugin for this browser.
-            That's ok, <a href={props.src}>click here to download the PDF file.</a></p>
-        </object>
-      </Row>
-    </Container>
-  );
+    <object data={props.data} type="application/pdf" width="100%" height="800px">
+      <p>It appears you don't have a PDF plugin for this browser.
+        That's ok, <a href={props.src}>click here to download the PDF file.</a></p>
+    </object>
+  ); 
 }
 
-ResumePage.propTypes = {
+ResumeObject.propTypes = {
   data: PropTypes.string,
   src: PropTypes.string,
 };
 
 export default function Resume(){
   return(
-    <ResumePage
+    <ResumeObject
       data="public_resume.pdf"
       src="public_resume.pdf"
     />
