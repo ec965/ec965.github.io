d0f37bda1d8e62ef0c928a97bdb9690ee317ce30
ec965 <enoch965@gmail.com>
2020-11-26 02:34:00 -0800

    fixed reel

--- src/fun/reel.js
+++ src/fun/reel.js
@@ -1,15 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {Figure, Video} from '../projects/page';
-import {CSSTransition} from 'react-transition-group';
 
 export default function Reel(props){
   const [slideIndex, setSlideIndex] = useState(0);
   const [lastSlide, setLastSlide] = useState(false);
   const [firstSlide, setFirstSlide] = useState(true);
-  const [fadeIn, setFadeIn] = useState(false);
   const {slides} = props;
   
-  const transistion_time = 500;
 
   useEffect(()=> {
     if(slideIndex === 0 ){
@@ -28,16 +25,12 @@ export default function Reel(props){
 
   function handlePrev(){
     if (slideIndex > 0){
-      setFadeIn(true);
       setSlideIndex(slideIndex-1);
-      setTimeout(()=>{setFadeIn(false);},transistion_time);
     }
   }
   function handleNext(){
     if (slideIndex < slides.length-1){
-      setFadeIn(true);
       setSlideIndex(slideIndex+1);
-      setTimeout(()=>{setFadeIn(false);},transistion_time);
     }
   }
   return(
@@ -47,22 +40,20 @@ export default function Reel(props){
           <i onClick={handlePrev} className="slide-button fas fa-caret-left"></i>
         )}
       </div>
-      <CSSTransition 
-        in={fadeIn} 
-        timeout={transistion_time} 
-        classNames={"slide-node"}
-      >
-        <div>
-          <Figure caption={slides[slideIndex].caption}>
-            {slides[slideIndex].type === 'image' && (
-              <img src={slides[slideIndex].media} alt={slides[slideIndex].alt} className="slide-image"/>
-            )}
-            {slides[slideIndex].type === 'video' && (
-              <Video className="slide-video" youtube vidsrc={slides[slideIndex].media}/>
-            )}
-          </Figure>
-        </div>
-      </CSSTransition>
+      <div >
+        {slides.map((slide, index) => 
+          (slideIndex === index) && (
+            <Figure caption={slide.caption} className={slideIndex===index && 'slide-node'}>
+              {slide.type === 'image' && (
+                <img src={slide.media} alt={slide.alt} className="slide-image"/>
+              )}
+              {slide.type === 'video' && (
+                <Video className="slide-video" youtube vidsrc={slide.media}/>
+              )}
+            </Figure>
+          )
+        )}
+      </div>
       <div className="slide-control">
         {!lastSlide && (
           <i onClick={handleNext} className="slide-button fas fa-caret-right"></i>
