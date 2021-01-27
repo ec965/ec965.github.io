24ec9c9dbe69deb533cec4a9fa4956fba5ae4be4
ec965 <enoch965@gmail.com>
2021-01-26 17:06:58 -0800

    Updates

---
+++ precache-manifest.07c75b5f938ecb4f2eed5bc80bf10203.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bde5704e06c032194420a32c8f199a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc79a78d6c4bf633702",
+    "url": "/static/css/main.35226872.chunk.css"
+  },
+  {
+    "revision": "cd2481d2484a86abd216",
+    "url": "/static/js/2.76a0d04d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76a0d04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc79a78d6c4bf633702",
+    "url": "/static/js/main.fbeb6004.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2c1d8ffaac8d1351e8f03a7944e9b426",
+    "url": "/static/media/mada-regular-webfont.2c1d8ffa.woff2"
+  },
+  {
+    "revision": "6e4ab6fa5506ac09a5bfa97418b500f5",
+    "url": "/static/media/mada-regular-webfont.6e4ab6fa.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.434ddf8ede20533ff04eb33c9b2a7033.js"
+  "/precache-manifest.07c75b5f938ecb4f2eed5bc80bf10203.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
