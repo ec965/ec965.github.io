4ddd41a000cee23067c54dca1cfc455b747a775a
ec965 <enoch965@gmail.com>
2021-05-23 22:49:31 -0700

    starting ts

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//css
-import './css/stylesheet.css';
-//fonts
-import './font/stylesheet.css';
-import App from './home/index.js';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
